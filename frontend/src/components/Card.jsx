f3035afd6317f461558980f670dbbd79479c820d
audiofilter <for38864@gmail.com>
2025-03-17 19:39:53 +0100

    added single coin page and all coins page3

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,12 +4,13 @@ import { RiUserLine } from "react-icons/ri";
 import { GoArrowRight } from "react-icons/go";
 import PrimaryButton from "./buttons/PrimaryButton";
 import SecondaryButton from "./buttons/SecondaryButton";
+import PrimaryButtonInvert from "./buttons/PrimaryButtonInvert";
 
 const Card = ({ image, name, ca, buyersPercent, marketCap, marketPercent }) => {
   return (
-    <div className="flex-shrink-0 cursor-pointer p-4 bg-white shadow-md flex flex-row md:flex-col md:flex-row gap-4 md:gap-0 items-center justify-left rounded-3xl scroll-snap-align-start bg-radial-[at_-300%_25%] md:bg-radial-[at_-300%_70%] from-[#FF860A] to-[#161616] to-65%">
+    <div className="flex-shrink-0 cursor-pointer p-3 md:p-4 bg-white shadow-md flex flex-row  gap-4 md:gap-0 items-center justify-left rounded-3xl scroll-snap-align-start bg-radial-[at_-300%_25%] md:bg-radial-[at_-300%_70%] from-[#FF860A] to-[#161616] to-65%">
       {/* Image */}
-      <div class="w-45 md:w-50 h-45 md:h-50 flex items-center justify-center overflow-hidden rounded-xl bg-gray-200">
+      <div class="w-40 md:w-50 h-40 md:h-50 flex items-center justify-center overflow-hidden rounded-xl bg-gray-200">
         <img
           src={image}
           alt="Memecoin"
@@ -18,7 +19,7 @@ const Card = ({ image, name, ca, buyersPercent, marketCap, marketPercent }) => {
         />
       </div>
       {/* Content */}
-      <div className="flex flex-col gap-1 mx-0 md:mx-5">
+      <div className="flex flex-col md:gap-1 mx-0 md:mx-5">
         <p className="text-lg font-medium">{name}</p>
         <div className="flex flex-row gap-1 mt-1">
           <p className="text-xs text-[#B0B0B0]">{`CA: ${ca?.slice(
@@ -41,8 +42,7 @@ const Card = ({ image, name, ca, buyersPercent, marketCap, marketPercent }) => {
         </div>
         <div className="flex flex-row gap-2 items-center mt-2">
           <PrimaryButton name="Trade" className2="bg-[#161616]" />
-          <SecondaryButton
-            className="text-xl p-3 px-5"
+          <PrimaryButtonInvert
             icon={<GoArrowRight />}
             href={`/coins/${ca}`}
           />
