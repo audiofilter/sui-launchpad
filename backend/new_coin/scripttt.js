6b6e06b91bab50b44c4f4056510accb6e820945c
audiofilter <for38864@gmail.com>
2025-03-24 08:15:48 +0100

    dump bytecode as base64 is the problem.

--- backend/new_coin/scripttt.js
+++ backend/new_coin/scripttt.js
@@ -3,44 +3,53 @@ const path = require("path");
 const { execSync } = require('child_process');
 const { Transaction } = require('@mysten/sui/transactions');
 
-const newCoin = (name,symbol, iconUrl, description) => {
-    // exports.newCoin = (name,symbol, iconUrl, description) => {
-    // const newCoin = (name ="BROWN",symbol ="BRN", iconUrl="example.com", description="A better version of Naira") => {
+const newCoin = (name, symbol, iconUrl, description) => {
+    // Replace spaces with underscores and convert to lowercase
     name = name.replace(" ", "_").toLowerCase();
     symbol = symbol.toUpperCase();
-    const currentPath = path.join(__dirname, "/");
-    execSync(`sui move new ${name}`);
+    
+    const rootPath = process.cwd();
+    console.log(rootPath);
+    const packagePath = path.join(rootPath, `${name}/`);
 
-    const templatePath = path.join(__dirname, "template.txt");
+    console.log(packagePath);
+
+    execSync(`sui move new ${name}`);
+    // execSync(`sui move build --path ./${name}`);
+
+    const templatePath = path.join(rootPath, "new_coin", "template.txt");
     let fileContent = fs.readFileSync(templatePath, "utf8");
     fileContent = fileContent.replace("template_description", description);
     fileContent = fileContent.replace(/TEMPLATE/g, symbol);
-    fileContent = fileContent.replace("Template", name.charAt(0).toUpperCase() + name.slice(1))
+    fileContent = fileContent.replace("Template", name.charAt(0).toUpperCase() + name.slice(1));
     fileContent = fileContent.replace(/template/g, name);
 
-    fs.writeFileSync(path.join(__dirname, `${name}/sources/${name}.move`), fileContent);
+    console.log(fileContent);
+    fs.writeFileSync(path.join(rootPath, `${name}/sources/${name}.move`), fileContent);
 
-    const packagePath = path.join(__dirname, `${name}/`);
     const { modules, dependencies } = JSON.parse(
-		execSync(`sui move build --dump-bytecode-as-base64 --path ${packagePath}`, {
-			encoding: 'utf-8',
-		}),
-	);
+        execSync(`sui move build --path ${packagePath} --install-dir ./${name}`, {
+            encoding: 'utf-8',
+        }),
+    );
+    
     const tx = new Transaction();
     const cap = tx.publish({
-		modules,
-		dependencies,
-	});
+        modules,
+        dependencies,
+    });
 
-	// Transfer the upgrade capability to the sender so they can upgrade the package later if they want.
-	tx.moveCall({
+    // Transfer the upgrade capability to the sender so they can upgrade the package later
+    tx.moveCall({
         target: "0x2::package::make_immutable",
         arguments: [
             cap,
         ]
     });
 
+    console.log(tx);
+    
     return tx;
 }
 
-newCoin("abuja", "ABUJA", "http://somewhere.com", "A better version of Naira");
+newCoin("abuja", "ABUJA", "http://somewhere.com", "A better version of Naira");	
